a5b66b4d3cdd9f65217909998a6cddef37b03104
jjdia <jeff.jdia@gmail.com>
2026-09-14 14:30:30 -0400

    Refresh cache for secure admin signup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v10";
+const CACHE="propertyworks-v11";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
