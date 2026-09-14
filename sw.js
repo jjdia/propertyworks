12f96e6f8f114404f4a1b8444b6235d774259cc9
jjdia <jeff.jdia@gmail.com>
2026-09-14 14:40:26 -0400

    Refresh cache for fixed admin account form

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v12";
+const CACHE="propertyworks-v13";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
