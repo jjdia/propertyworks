e1f148132aeba6cb521950f7c88b0e00d7cef2ec
jjdia <jeff.jdia@gmail.com>
2026-09-14 13:43:19 -0400

    Refresh PropertyWorks cache for notifications and automation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v4";
+const CACHE="propertyworks-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
