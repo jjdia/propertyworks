eca8fe658e9d34ac0c92228487fd88814dce9cdb
jjdia <jeff.jdia@gmail.com>
2026-09-14 14:49:56 -0400

    Refresh cache for account profile center

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v13";
+const CACHE="propertyworks-v14";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
