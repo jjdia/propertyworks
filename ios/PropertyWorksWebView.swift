dad34cd2d15f92f21e19d66150971c924cdb7dc7
jjdia <jeff.jdia@gmail.com>
2026-09-14 08:36:29 -0400

    Add ios/PropertyWorksWebView.swift

---
+++ ios/PropertyWorksWebView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import WebKit
+
+struct PropertyWorksWebView: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.allowsBackForwardNavigationGestures = true
+        if let url = URL(string: "https://jjdia.github.io/propertyworks/") {
+            webView.load(URLRequest(url: url))
+        }
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
