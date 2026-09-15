5a78bd5e71161b3f2b097e63a5d3db9ec04662ee
jjdia <jeff.jdia@gmail.com>
2026-09-15 09:19:01 -0400

    Refresh cache for expanded property expense tracking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v23";
+const CACHE="propertyworks-v24";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
