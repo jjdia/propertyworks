090728e67a07bf6493bfe55acc39902b12647529
jjdia <jeff.jdia@gmail.com>
2026-09-15 08:53:58 -0400

    Refresh cache for rent backfill and garage tracking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="propertyworks-v22";
+const CACHE="propertyworks-v23";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(
